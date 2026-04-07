3fb4296fda18924b7ca558b8e166f7726696beee
yeonjeen <jeendev2019@gmail.com>
2026-04-08 00:04:25 +0900

    refactor: 보관함 zustand 스토어 단순화

--- store/useStorageStore.ts
+++ store/useStorageStore.ts
@@ -1,24 +1,11 @@
 import { create } from "zustand";
 
 interface UseStorage {
-  claimedLevels: number[];
-  equippedLevel: number | null;
   shouldReopenReward: boolean;
-  claim: (level: number) => void;
-  setEquipped: (level: number | null) => void;
   setShouldReopenReward: (value: boolean) => void;
 }
 
 export const useStorageStore = create<UseStorage>((set) => ({
-  claimedLevels: [],
-  equippedLevel: null,
   shouldReopenReward: false,
-  claim: (level) =>
-    set((state) => ({
-      claimedLevels: state.claimedLevels.includes(level)
-        ? state.claimedLevels
-        : [...state.claimedLevels, level],
-    })),
-  setEquipped: (level) => set({ equippedLevel: level }),
   setShouldReopenReward: (value) => set({ shouldReopenReward: value }),
 }));
